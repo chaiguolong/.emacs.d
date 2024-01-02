7962fec55b4b1ab2fc42505bc3f05d70353c1f43
chaiguolong <584238433@qq.com>
2024-01-02 10:45:35 +0800

    修改了evil,定制化了evil-snip的颜色

--- custom.el
+++ custom.el
@@ -6,11 +6,12 @@
  '(custom-safe-themes
    '("88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" default))
  '(package-selected-packages
-   '(undo-tree org-contrib org airline-themes gruvbox-theme doom-modeline magit wgrep embark-consult consult cosult embark marginalia orderless vertico keycast company))
+   '(evil-anzu undo-tree org-contrib org airline-themes gruvbox-theme doom-modeline magit wgrep embark-consult consult cosult embark marginalia orderless vertico keycast company))
  '(powerline-default-separator 'bar))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(evil-snipe-first-match-face ((t (:inherit isearch))))
+ '(evil-snipe-matches-face ((t (:inherit region :background "DarkOrange3")))))
--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -13,6 +13,28 @@
   ;;将esc绑定到evil-insert-state-map
   (define-key evil-insert-state-map [escape] 'evil-normal-state)
 
+  ;;新增空行
+  (define-key evil-normal-state-map (kbd "[ SPC") (lambda () (interactive) (evil-insert-newline-above) (forward-line)))
+  (define-key evil-normal-state-map (kbd "] SPC") (lambda () (interactive) (evil-insert-newline-below) (forward-line -1)))
+
+  ;;qiehuan buffer
+  (define-key evil-normal-state-map (kbd "[ b") 'previous-buffer)
+  (define-key evil-normal-state-map (kbd "] b") 'next-buffer)
+  (define-key evil-motion-state-map (kbd "[ b") 'previous-buffer)
+  (define-key evil-motion-state-map (kbd "] b") 'next-buffer)
+
+  ;;普通模式下dired的按键绑定
+  (evil-define-key 'normal dired-mode-map
+  ;;打开当期文件
+  (kbd "<RET>") 'dired-find-alternate-file
+  ;;进入上一级目录
+  (kbd "C-k") 'dired-up-directory
+  "`" 'dired-open-term
+  "q" 'quit-window
+  ;;在另一个窗口打开当前文件
+  "o" 'dired-find-file-other-window
+  ")" 'dired-omit-mode)
+
 
   ;; https://emacs.stackexchange.com/questions/46371/how-can-i-get-ret-to-follow-org-mode-links-when-using-evil-mode
   (with-eval-after-load 'evil-maps
@@ -28,14 +50,49 @@
   (setq undo-tree-auto-save-history nil)
   (evil-set-undo-system 'undo-tree))
 
+;;使用*对当前词语进行搜索的时候,可以在modeline上显示总共有多少个结果,以及当前在哪个结果上
+(use-package evil-anzu
+  :ensure t
+  :after evil
+  :diminish
+  :demand t
+  :init
+  (global-anzu-mode t))
 
-;;(use-package evil
-;;  :ensure t
-;;
-;;  :init
-;;  ;; Enable Evil
-;;  (require 'evil)
-;;  (evil-mode 1)
-;;  )
+;;社区贡献的按键绑定,有较强的合理性
+(use-package evil-collection
+  :ensure t
+  :demand t
+  :config
+  ;;移除一些个性化的按键绑定
+  (setq evil-collection-mode-list (remove 'lispy evil-collection-mode-list))
+  (evil-collection-init)
+
+  (cl-loop for (mode . state) in
+	   '((org-agenda-mode . normal)
+	     (Custom-mode . emacs)
+	     (eshell-mode . emacs)
+	     (makey-key-mode . motion))
+	   do (evil-set-initial-state mode state)))
+
+
+;;注释插件
+(use-package evil-nerd-commenter
+  :init
+  (define-key evil-normal-state-map (kbd ",/") 'evilnc-comment-or-uncomment-lines)
+  (define-key evil-visual-state-map (kbd ",/") 'evilnc-comment-or-uncomment-lines)
+  )
+
+
+;;用f去查找字符,会高亮所有结果,然耨用;移动到下一个
+(use-package evil-snipe
+  :ensure t
+  :diminish
+  :init
+  (evil-snipe-mode +1)
+  (evil-snipe-override-mode +1)
+  )
+
+
 
 (provide 'init-evil)
