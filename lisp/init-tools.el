c7b33ec77c916dd4c220f26b1549793d8922d231
chaiguolong <584238433@qq.com>
2024-05-08 18:04:34 +0800

    first commit

--- lisp/init-tools.el
+++ lisp/init-tools.el
@@ -117,6 +117,9 @@
   :init
   (setq google-translate-translation-directions-alist '(("en" . "zh-CN"))))
 
+;;emacsGUI设置代理
+(setq url-gateway-method 'socks)
+(setq socks-server '("Default server" "127.0.0.1" 1081 5))
 
 (provide 'init-tools)
 
