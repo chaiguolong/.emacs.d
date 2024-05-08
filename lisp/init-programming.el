9e7be1f83917e7fe28f9fe5bcb074e92818ed9fc
chaiguolong <584238433@qq.com>
2024-05-08 22:03:34 +0800

    delete treesitter

--- lisp/init-programming.el
+++ lisp/init-programming.el
@@ -1,15 +1,5 @@
 (require 'eglot)
 
-(use-package treesit-auto
-  :demand t
-  :init
-  (progn
-    (setq treesit-font-lock-level 4)
-    )
-  :config
-  (setq treesit-auto-install 'prompt)
-  (global-treesit-auto-mode))
-
 
 
 
