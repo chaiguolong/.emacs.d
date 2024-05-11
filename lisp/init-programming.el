d12415bfb5e5e2bede933f53bd303cc64d1fe9dd
chaiguolong <584238433@qq.com>
2024-05-12 00:38:19 +0800

    first commit

--- lisp/init-programming.el
+++ lisp/init-programming.el
@@ -1,19 +1,5 @@
 (require 'eglot)
 
-<<<<<<< HEAD
-;;(use-package treesit-auto
-;;  :demand t
-;;  :init
-;;  (progn
-;;    (setq treesit-font-lock-level 4)
-;;    )
-;;  :config
-;;  (setq treesit-auto-install 'prompt)
-;;  (global-treesit-auto-mode))
-
-=======
->>>>>>> e1561d480699a3177e70c69f912af3b935e6c061
-
 
 
 
