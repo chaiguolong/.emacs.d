aa874586cbc369ed788e59f4f98d6463f23e040a
chaiguolong <584238433@qq.com>
2025-10-11 03:13:16 +0800

    打开当前目录spc b f

--- lisp/init-funcs.el
+++ lisp/init-funcs.el
@@ -26,6 +26,20 @@ Supports exporting consult-grep to wgrep, file to wdeired, and consult-location 
   (consult-directory-externally default-directory))
 
 
+(defun consult-directory-externally (file)
+  "Open FILE externally using the default application of the system."
+  (interactive "fOpen externally: ")
+  (if (and (eq system-type 'windows-nt)
+           (fboundp 'w32-shell-execute))
+      (shell-command-to-string (encode-coding-string (replace-regexp-in-string "/" "\\\\" (format "explorer.exe %s" (file-name-directory (expand-file-name file)))) 'gbk))
+    (call-process (pcase system-type
+                    ('darwin "open")
+                    ('cygwin "cygstart")
+                    (_ "xdg-open"))
+                  nil 0 nil
+                  (file-name-directory (expand-file-name file)))))
+
+
 
 
 
